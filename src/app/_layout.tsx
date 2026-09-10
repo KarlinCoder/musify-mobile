62f9b50e3e29c8bdb13b87c841281a68e140f283
KarlinCoder <giancarlo.frontend@gmail.com>
2026-09-10 11:45:52 -0400

    second commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
@@ -7,6 +7,7 @@ export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <Text className="w-40 text-5xl bg-red-600">Hola mundo</Text>
+      <Slot />
     </SafeAreaProvider>
   );
 }
